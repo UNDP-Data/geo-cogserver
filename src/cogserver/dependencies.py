0aa7ea27780b19041a26b8eede8712346898d1ef
janf <ioan.ferencik@solargis.com>
2023-11-03 22:17:24 +0100

    added MosaicJson  create mosaic endpoints as an extension

--- src/cogserver/dependencies.py
+++ src/cogserver/dependencies.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI, Query, Depends, Request
 from typing_extensions import Annotated
+from typing import List
 import base64
+
+def parse_signed_url(url:str=None):
+
+    if '?' in url:
+        furl, b64token = url.split('?')
+        try:
+            decoded_token = base64.b64decode(b64token).decode()
+        except Exception:
+            decoded_token = b64token
+        decoded_url =  f'{furl}?{decoded_token}'
+    else:
+        decoded_url =  f'{url}'
+    return decoded_url
+
+
 def SignedDatasetPath(url: Annotated[str, Query(description="Unsigned/signed dataset URL")]) -> str:
     """
         FastAPI dependency function that enables
@@ -24,14 +40,32 @@ def SignedDatasetPath(url: Annotated[str, Query(description="Unsigned/signed dat
         (spatial align, resolution, resmapling) that, to some extent can influence the performace of the server
 
     """
-    if '?' in url:
-        furl, b64token = url.split('?')
-        try:
-            decoded_token = base64.b64decode(b64token).decode()
-        except Exception:
-            decoded_token = b64token
-        decoded_url =  f'{furl}?{decoded_token}'
-    else:
-        decoded_url =  f'{url}'
-    return decoded_url
+    return parse_signed_url(url=url)
 
+def SignedDatasetPaths(url: Annotated[List[str], Query(description="Unsigned/signed dataset URLs")]) -> str:
+    """
+        FastAPI dependency function that enables
+        COG endpoint to handle Azure Blob located&signed raster files
+        and combine them into one  multiband GDAL VRT file.
+        This allows one to use titiler's  expression parameter
+        to perform simple but powerful multiband computations.
+        Example
+            expression=((b1>0.5)&(b3<100))/b4;
+
+        Each url consists of a base64 encoded SAS token:
+
+        http://localhost:8000/cog/statistics? \
+        url=https://undpngddlsgeohubdev01.blob.core.windows.net/testforgeohub/HREA_Algeria_2012_v1%2FAlgeria_rade9lnmu_2012.tif?c3Y9MjAyMC0xMC0wMiZzZT0yMDIyLTAzLTA0VDE0JTNBMzIlM0E0M1omc3I9YiZzcD1yJnNpZz0lMkJUZVd0UnRScG1uNmpTQ1ZHY2JuV3dhUWMlMkJjRlp5c05ENjRDUDlyMERNRSUzRA==& \
+        url=https://undpngddlsgeohubdev01.blob.core.windows.net/testforgeohub/HREA_Algeria_2012_v1%2FAlgeria_set_zscore_sy_2012.tif?c3Y9MjAyMC0xMC0wMiZzZT0yMDIyLTAzLTA0VDE0JTNBMzIlM0E0M1omc3I9YiZzcD1yJnNpZz1DTFRBQmI5aVRmV3UlMkZ0VHElMkZ4MCUyQm1hUDFUYVM5eUtDMnB3UTBjOUZmMlNBJTNE \
+
+        The returned value is a str representing a RAM stored GDAL VRT file
+        which Titiler will use to resolve the request
+
+        Obviously the rasters need to spatially overlap. Additionaly, the VRT can be created with various params
+        (spatial align, resolution, resmapling) that, to some extent can influence the performace of the server
+
+    """
+    decoded_urls = list()
+    for src_url in url:
+        decoded_urls.append(parse_signed_url(url=src_url))
+    return decoded_urls
--- src/cogserver/extensions.py
+++ src/cogserver/extensions.py
@@ -1,14 +1,64 @@
 from dataclasses import dataclass, field
-from typing import Tuple, List, Optional
-
-import rasterio
-from starlette.responses import Response
+from typing import  List, Optional
+from cogserver.dependencies import SignedDatasetPaths
+from typing_extensions import Annotated
 from fastapi import Depends, FastAPI, Query
-from titiler.core.factory import BaseTilerFactory, FactoryExtension, TilerFactory
-from titiler.core.dependencies import RescalingParams
-from titiler.core.factory import TilerFactory
-from titiler.core.resources.enums import ImageType
+from titiler.core.factory import BaseTilerFactory, FactoryExtension
+from cogeo_mosaic.mosaic import MosaicJSON
+from titiler.core.resources.responses import JSONResponse
+from pydantic import BaseModel
+
+urls = Annotated[List[str], Query(..., description="Dataset URLs")]
+class MosaicJsonCreateItem(BaseModel):
+    #url: List[str] = Query(..., description="Dataset URL")
+    url:List[str] = urls
+    minzoom: int = 0
+    maxzoom: int = 22
+    attribution: str = None
+
 
 @dataclass
 class createMosaicJsonExtension(FactoryExtension):
-    pass
+
+    def create_mosaic_json(self, urls=None, minzoom=None, maxzoom=None, attribution=None):
+        mosaicjson = MosaicJSON.from_urls(urls=urls, minzoom=minzoom, maxzoom=maxzoom, )
+        if attribution is not None:
+            mosaicjson.attribution = attribution
+        return mosaicjson
+
+    # Register method is mandatory and must take a BaseTilerFactory object as input
+    def register(self, factory: BaseTilerFactory):
+        @factory.router.get(
+            "/create",
+            response_model=MosaicJSON,
+            response_model_exclude_none=True,
+            response_class=JSONResponse,
+            responses={
+                200: {"description": "Return a MosaicJSON from multiple COGs."}},
+        )
+        def create_mosaicJSON(
+                url = Depends(SignedDatasetPaths),
+                minzoom: Optional[int] = 0,
+                maxzoom: Optional[int] = 22,
+                attribution: Optional[str] = None
+
+        ):
+            return self.create_mosaic_json(urls=url, minzoom=minzoom, maxzoom=maxzoom, attribution=attribution)
+
+        @factory.router.post(
+            "/create",
+            response_model=MosaicJSON,
+            response_model_exclude_none=True,
+            response_class=JSONResponse,
+            responses={
+                200: {"description": "Return a MosaicJSON from multiple COGs."}},
+        )
+        def create_mosaicJSON(payload: MosaicJsonCreateItem):
+            url = SignedDatasetPaths(payload.urls)
+            minzoom = payload.minzoom
+            maxzoom = payload.maxzoom
+            attribution = payload.attribution
+
+            return self.create_mosaic_json(urls=url,minzoom=minzoom, maxzoom=maxzoom, attribution=attribution)
+
+
