01231e784a9fd2e199c7ab9cc7dcd57e64a43d17
janf <ioan.ferencik@solargis.com>
2024-03-25 17:44:29 +0100

    add colormap endpoint

--- src/app.py
+++
@@ -1,6 +0,0 @@
-
-import uvicorn
-
-
-if __name__ == '__main__':
-    uvicorn.run(app='cogserver:app', host="127.0.0.1", port=8001, log_config="src/cogserver/logconf.yaml", reload=True, reload_dirs=['/home/janf/.local/share/virtualenvs/geo-cogserver-Wjrq5VZr/lib/python3.10/site-packages/titiler/', './'] )
