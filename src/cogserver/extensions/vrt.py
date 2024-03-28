87859acdb2cb38e2b8d5efd4272712d52382b296
Jin Igarashi <igarashi.jin@water-gis.com>
2024-03-28 16:44:47 +0000

    fix: order of datatypes

--- src/cogserver/extensions/vrt.py
+++ src/cogserver/extensions/vrt.py
@@ -60,10 +60,10 @@ def create_vrt_from_urls(
 
         data_types = [
             "Byte",
-            "UInt16", "Int16", "UInt32", "Int32",
-            "Float32", "Float64",
-            "CInt16", "CInt32",
-            "CFloat32", "CFloat64"
+            "UInt16", "Int16", "CInt16",
+            "UInt32", "Int32", "CInt32"
+            "Float32", "CFloat32",
+            "Float64", "CFloat64"
         ]
 
         with open(temp.name, "r") as file:
