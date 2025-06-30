28d42b3d34649989835a463e8a30214a171f5cc1
Jin Igarashi <igarashi.jin@water-gis.com>
2025-06-30 20:17:02 +0900

    fix: fixed landing page and upgraded base docker image to gdal 3.11 (#119)
    * fix: apply the latest landing page code from titiler.
    
    * feat: upgade gdal base image from 3.8 to 3.11

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
-FROM ghcr.io/osgeo/gdal:ubuntu-small-3.8.4 as base
+FROM ghcr.io/osgeo/gdal:ubuntu-small-3.11.0
 
-RUN apt-get update \
-  && apt-get install -y --no-install-recommends \
-  libffi-dev python3-pip
-RUN python3 -m pip install pipenv
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+      libffi-dev \
+      python3-pip \
+      pipenv
+
 WORKDIR /opt/server
+
 RUN export PYTHON_VERSION="$(python3 --version | cut -d ' ' -f 2)" && pipenv --python ${PYTHON_VERSION} --site-packages
+
 RUN pipenv run pip install -U pip
+
 #RUN pipenv run pip install uvicorn titiler asyncpg postgis --no-cache-dir  --upgrade
 COPY requirements.txt requirements.txt
 RUN pipenv run pip install -r requirements.txt
--- src/cogserver/server.py
+++ src/cogserver/server.py
@@ -1,14 +1,19 @@
+from typing import Annotated, Literal, Optional
 from titiler.application import main as default
 from cogserver.dependencies import SignedDatasetPath
 from cogserver.algorithms import algorithms
 from rio_tiler.io import STACReader
+from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
 import logging
 import rasterio
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from titiler.core.factory import TilerFactory, MultiBaseTilerFactory, AlgorithmFactory, ColorMapFactory
 from titiler.application import __version__ as titiler_version
-from cogserver.landing import setup_landing
-from starlette.middleware.cors import CORSMiddleware
+from titiler.core.models.OGC import Landing, Conformance
+from titiler.core.resources.enums import MediaType
+from titiler.core.templating import create_html_response
+from titiler.core.utils import accept_media_type, update_openapi
 from titiler.mosaic.factory import MosaicTilerFactory
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 from titiler.mosaic.errors import MOSAIC_STATUS_CODES
@@ -26,7 +31,7 @@ api_settings = default.api_settings
 app = FastAPI(
     title=api_settings.name,
     openapi_url="/api",
-    docs_url="/api.html",
+    docs_url="/index.html",
     description="""A modern dynamic tile server built on top of FastAPI and Rasterio/GDAL.
 ---
 
@@ -39,6 +44,17 @@ app = FastAPI(
     version=titiler_version,
     root_path=api_settings.root_path,
 )
+
+# Fix OpenAPI response header for OGC Common compatibility
+update_openapi(app)
+
+TITILER_CONFORMS_TO = {
+    "http://www.opengis.net/spec/ogcapi-common-1/1.0/req/core",
+    "http://www.opengis.net/spec/ogcapi-common-1/1.0/req/landing-page",
+    "http://www.opengis.net/spec/ogcapi-common-1/1.0/req/oas30",
+    "http://www.opengis.net/spec/ogcapi-common-1/1.0/req/html",
+    "http://www.opengis.net/spec/ogcapi-common-1/1.0/req/json",
+}
 
 ###############################################################################
 
@@ -55,7 +71,7 @@ cog = TilerFactory(
     process_dependency=algorithms.dependency
 )
 app.include_router(cog.router, prefix="/cog", tags=["Cloud Optimized GeoTIFF"])
-
+TITILER_CONFORMS_TO.update(cog.conforms_to)
 ###############################################################################
 
 ############################# MosaicJSON ######################################
@@ -70,7 +86,7 @@ mosaic = MosaicTilerFactory(
     ]
 )
 app.include_router(mosaic.router, prefix="/mosaicjson", tags=["MosaicJSON"])
-
+TITILER_CONFORMS_TO.update(mosaic.conforms_to)
 ###############################################################################
 
 
@@ -91,6 +107,7 @@ stac = MultiBaseTilerFactory(
 app.include_router(
     stac.router, prefix="/stac", tags=["SpatioTemporal Asset Catalog"]
 )
+TITILER_CONFORMS_TO.update(stac.conforms_to)
 
 ###############################################################################
 
@@ -109,6 +126,7 @@ app.include_router(
     algorithmsFactory.router, tags=["Algorithms"]
 )
 
+TITILER_CONFORMS_TO.update(algorithmsFactory.conforms_to)
 
 ############################## VRT ###################################
 
@@ -122,7 +140,7 @@ vrt = VRTFactory(
 )
 
 app.include_router(vrt.router, prefix="/vrt", tags=["VRT"])
-
+TITILER_CONFORMS_TO.update(vrt.conforms_to)
 
 
 ###############################################################################
@@ -141,6 +159,7 @@ app.include_router(
     cmaps.router,
     tags=["ColorMaps"],
 )
+TITILER_CONFORMS_TO.update(cmaps.conforms_to)
 
 ###############################################################################
 
@@ -159,7 +178,150 @@ def ping():
     }
 
 
-setup_landing(app)
+@app.get(
+    "/",
+    response_model=Landing,
+    response_model_exclude_none=True,
+    responses={
+        200: {
+            "content": {
+                "text/html": {},
+                "application/json": {},
+            }
+        },
+    },
+    tags=["OGC Common"],
+)
+def landing(
+    request: Request,
+    f: Annotated[
+        Optional[Literal["html", "json"]],
+        Query(
+            description="Response MediaType. Defaults to endpoint's default or value defined in `accept` header."
+        ),
+    ] = None,
+):
+    """TiTiler landing page."""
+    data = {
+        "title": "geo-cogserver",
+        "description": "A TiTiler extension for UNDP GeoHub which is a modern dynamic tile server built on top of FastAPI and Rasterio/GDAL.",
+        "links": [
+            {
+                "title": "Landing page",
+                "href": str(request.url_for("landing")),
+                "type": "text/html",
+                "rel": "self",
+            },
+            {
+                "title": "The API definition (JSON)",
+                "href": str(request.url_for("openapi")),
+                "type": "application/vnd.oai.openapi+json;version=3.0",
+                "rel": "service-desc",
+            },
+            {
+                "title": "The API documentation",
+                "href": str(request.url_for("swagger_ui_html")),
+                "type": "text/html",
+                "rel": "service-doc",
+            },
+            {
+                "title": "Conformance Declaration",
+                "href": str(request.url_for("conformance")),
+                "type": "text/html",
+                "rel": "http://www.opengis.net/def/rel/ogc/1.0/conformance",
+            },
+            {
+                "title": "geo-cogserver source code (external link)",
+                "href": "https://github.com/UNDP-Data/geo-cogserver",
+                "type": "text/html",
+                "rel": "doc",
+            },
+            {
+                "title": "TiTiler Documentation (external link)",
+                "href": "https://developmentseed.org/titiler/",
+                "type": "text/html",
+                "rel": "doc",
+            },
+            {
+                "title": "TiTiler source code (external link)",
+                "href": "https://github.com/developmentseed/titiler",
+                "type": "text/html",
+                "rel": "doc",
+            },
+        ],
+    }
+
+    output_type: Optional[MediaType]
+    if f:
+        output_type = MediaType[f]
+    else:
+        accepted_media = [MediaType.html, MediaType.json]
+        output_type = accept_media_type(
+            request.headers.get("accept", ""), accepted_media
+        )
+
+    if output_type == MediaType.html:
+        return create_html_response(
+            request,
+            data,
+            title="TiTiler",
+            template_name="landing",
+        )
+
+    return data
+
+
+@app.get(
+    "/conformance",
+    response_model=Conformance,
+    response_model_exclude_none=True,
+    responses={
+        200: {
+            "content": {
+                "text/html": {},
+                "application/json": {},
+            }
+        },
+    },
+    tags=["OGC Common"],
+)
+def conformance(
+    request: Request,
+    f: Annotated[
+        Optional[Literal["html", "json"]],
+        Query(
+            description="Response MediaType. Defaults to endpoint's default or value defined in `accept` header."
+        ),
+    ] = None,
+):
+    """Conformance classes.
+
+    Called with `GET /conformance`.
+
+    Returns:
+        Conformance classes which the server conforms to.
+
+    """
+    data = {"conformsTo": sorted(TITILER_CONFORMS_TO)}
+
+    output_type: Optional[MediaType]
+    if f:
+        output_type = MediaType[f]
+    else:
+        accepted_media = [MediaType.html, MediaType.json]
+        output_type = accept_media_type(
+            request.headers.get("accept", ""), accepted_media
+        )
+
+    if output_type == MediaType.html:
+        return create_html_response(
+            request,
+            data,
+            title="Conformance",
+            template_name="conformance",
+        )
+
+    return data
 
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
 add_exception_handlers(app, MOSAIC_STATUS_CODES)
